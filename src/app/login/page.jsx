1504c0716d33781c233341df224b015919de61e5
Neelpaban Sarmah <neelpabansarmah@Neelpabans-MacBook-Pro.local>
2026-02-25 17:18:35 +0530

    login page header issue solved

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,20 +2,18 @@
 "use client";
 
 import { Suspense } from "react";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
 import LoginClient from "./LoginClient";
 
 export default function LoginPage() {
   return (
     <>
-      <Header />
+      
 
       <Suspense fallback={null}>
         <LoginClient />
       </Suspense>
 
-      <Footer />
+      
     </>
   );
 }
